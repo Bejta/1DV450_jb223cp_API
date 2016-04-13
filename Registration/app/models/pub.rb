bb7383f0f5bd4558def885b9828b2e6462f70ae5
Jasmin Bejtovic <jb223cp@student.lnu.se>
2016-04-13 01:29:04 +0000

    refactor: Some changes on database and seed file

--- Registration/app/models/pub.rb
+++ Registration/app/models/pub.rb
@@ -2,7 +2,6 @@ class Pub < ActiveRecord::Base
     
     validates :name, presence: true, length: { maximum: 50}
     validates :description, presence: true, length: { maximum: 500}
-    validates :event, presence: true, length: { maximum: 200}
     validates :rating, presence: true, :numericality => {:greater_than_or_equal => 1, :less_than => 6}
     validates :address, presence: true
     validates :creator_id,  presence: true
