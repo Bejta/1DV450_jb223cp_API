5f867fd0724b69011cdee057036d3b00341d66ed
Jasmin Bejtovic <jb223cp@student.lnu.se>
2016-06-08 22:52:21 +0000

    feat: create on tags. Should be finish with controllers

--- Registration/app/controllers/api/v1/tags_controller.rb
+++ Registration/app/controllers/api/v1/tags_controller.rb
@@ -3,6 +3,10 @@ module Api
    class TagsController < ApibaseController
        respond_to :json
        
+       skip_before_action :authenticate, only: [:index, :show]
+       before_action :offset_params, only: [:index]
+
+       
        def index 
          if (params[:pub_id])
             pub = Pub.find_by_id(params[:pub_id])
@@ -18,7 +22,6 @@ module Api
        end
        
        def show
-        
         tag = Tag.find_by_id(params[:id])
         if tag.present?
           respond_with tag, status: :ok
@@ -27,8 +30,34 @@ module Api
         end
        end
        
-       
-       
+       def create
+          if params[:pub_id].nil?
+            render json: { error: "There is no pub so that tag can be added to" } and return
+          end
+          
+          pub = Pub.find_by_id(params[:pub_id])
+          render json: { error: "There is no pub with id #{params[:pub_id]}" } and return unless pub.present?
+
+          pub = Pub.find_by_id(params[:pub_id])
+          
+          
+          begin
+             if Tag.find_by(name: tag_params[:name])
+               tag = Tag.find_by(name: tag_params[:name])
+             else
+               tag = Tag.new(tag_params)
+             end
+          if tag.save
+             pub.tags << tag
+             render json: tag, status: 201, location: [:api, tag]
+          else
+             render json: { errors: tag.errors }, status: :bad_request
+          end
+          rescue JSON::ParserError => e
+             render json: { error: "Could not parse JSON" }, status: :bad_request
+          end
+       end
+
      private  
        def tag_params
         json_params = ActionController::Parameters.new(JSON.parse(request.body.read))
