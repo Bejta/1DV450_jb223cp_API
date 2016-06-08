6fc8677589fc4ccf0dd7c08579000c83294dfc4d
Jasmin Bejtovic <jb223cp@student.lnu.se>
2016-06-08 22:20:49 +0000

    feat: Tags controller, index and show actions implemented

--- Registration/app/controllers/api/v1/tags_controller.rb
+++ Registration/app/controllers/api/v1/tags_controller.rb
@@ -4,10 +4,32 @@ module Api
        respond_to :json
        
        def index 
-           respond_with Tag.all , status: :ok
+         if (params[:pub_id])
+            pub = Pub.find_by_id(params[:pub_id])
+            if pub.present?
+              tags =  pub.tags
+            else
+            render json: { error: "Could not found pub with id #{params[:pub_id]}" }, status: :not_found and return
+            end
+         else
+            tags = Tag.all
+         end
+           respond_with tags, status: :ok
+       end
+       
+       def show
+        
+        tag = Tag.find_by_id(params[:id])
+        if tag.present?
+          respond_with tag, status: :ok
+        else
+          render json: { error: "Could not find tag with id of #{params[:id]}" }, status: :not_found
+        end
        end
        
        
+       
+     private  
        def tag_params
         json_params = ActionController::Parameters.new(JSON.parse(request.body.read))
         json_params.require(:tag).permit(:name)
